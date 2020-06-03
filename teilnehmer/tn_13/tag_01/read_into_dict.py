07aba00c5aec6ee55d562016adc2dfe502b158a5
TN13 <tn13@kurs.ucit>
2020-06-03 14:04:29 +0200

    Nicholas Files Tag1 - read_into_dict.py und hello.py

---
+++ teilnehmer/tn_13/tag_01/read_into_dict.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def create_dict(path_file):
+    list = []
+    with open(path_file,"r") as f:
+        for line in f:
+            if line[0] != "#":
+                list.append(line[:-2].split(";"))
+
+    return list
+
+def search
+
+def main():
+    file_path = "/home/coder/python_fortgeschritten/materialien/openthesaurus.txt"
+    my_list = create_dict(file_path)
+    for x in my_list:
+        print(x)
+
+if __name__ == "__main__":
+    main()
+    
+            
