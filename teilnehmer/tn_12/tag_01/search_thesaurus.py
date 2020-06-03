29f517a568cc8cd822a2c528207c2322837a84be
TN12 <tn12@kurs.ucit>
2020-06-03 14:04:41 +0200

    tag1.1

---
+++ teilnehmer/tn_12/tag_01/search_thesaurus.py
@@ -0,0 +1,16 @@
+def get_my_list(filepath):
+    with open(filepath, 'r') as f:
+        rohdaten = [line.strip() for line in f if not '#' in line]
+        arbeitsdaten = [i.split(';') for i in rohdaten]
+    return arbeitsdaten
+
+def search_list(my_list, search_val):
+    for i in my_list:
+        if search_val in i:
+            
+
+
+if __name__ == "__main__":
+    arbeitsdaten = get_my_list('/home/coder/python_fortgeschritten/materialien/openthesaurus.txt')
+    searcherg = search_list(arbeitsdaten, 'verpassen')
+    print(searcherg)
