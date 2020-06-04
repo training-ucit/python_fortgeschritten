7d8150c1ffb72417af2ca06fa12acc06d472158c
TRAINER <trainer@kurs.ucit>
2020-06-04 13:13:30 +0200

    Tag 2 - Beispiele

--- trainer/tag_02/class_sample.py
+++ trainer/tag_02/class_sample.py
@@ -6,36 +6,23 @@ class Person:
     template = "Name: {} - Ort: {}"
 
     def __init__(self, name=None, ort=None):
-        """ Description
-        :type self:
-        :param self:
-    
-        :type name:
-        :param name:
-    
-        :type ort:
-        :param ort:
-    
-        :raises:
-    
-        :rtype:
-        :returns:
-        """
         self.name = name
         self.ort = ort
 
     def get_adress_aufkleber(self):
-        """ Description
-        :type self:
-        :param self:
-    
-        :raises:
-    
-        :rtype:
-        :returns:
-        """    
         return self.template.format(self.name, self.ort)
 
+    def __str__(self):
+        return self.template.format(self.name, self.ort)
+
+    def __repr__(self):
+        return "Person('{}', '{}')".format(self.name, self.ort)
+
+    def __geheim(self):
+        pass
+
+    def _privat(self):
+        pass
 
 # Testbed
 if __name__ == "__main__":
@@ -45,13 +32,21 @@ if __name__ == "__main__":
 
     p2 = Person("Heinz", "Köln")
 
-    print(p.name, p.ort)
-    print(p2.name, p2.ort)
+    print(p)
+    print(p.__repr__())
 
-    print(p2.get_adress_aufkleber())
+    p3 = eval(p.__repr__())
+    print(p3)
 
-    p_daten = []
-    werte = dict(Klaus="Hamburg", Heinz="Wedel", Ulrich="Bukarest")
-    for k, v in werte.items():
-        p_daten.append(Person(k, v))
-    print(p_daten[1].get_adress_aufkleber())
+    p3._privat()
+    #p3.__geheim()
+    #print(p.name, p.ort)
+    #print(p2.name, p2.ort)
+
+    #print(p2.get_adress_aufkleber())
+
+    #p_daten = []
+    #werte = dict(Klaus="Hamburg", Heinz="Wedel", Ulrich="Bukarest")
+    #for k, v in werte.items():
+    #    p_daten.append(Person(k, v))
+    #print(p_daten[1].get_adress_aufkleber())
